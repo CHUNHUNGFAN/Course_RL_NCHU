397e6b8c02d42f6545907170388f07b0234b09c0
Jonathanfan <Jonathanfan@jonathanfanmacbook.local>
2018-09-26 22:59:45 +0800

    [ADD] Add algorithm.py

---
+++ src/algorithm.py
@@ -0,0 +1,14 @@
+def Algorithm():
+    return "Hello , this is Algorithm"
+
+def eGreddy():
+    return "This is eGreedy"
+
+def UCB1():
+    return "This is UCB1"
+
+def TS():
+    return "This is TS"
+
+if __name__ == '__main__':
+    pass
--- src/test.py
+++ src/test.py
@@ -1,1 +1,21 @@
-print("Hello")
+import numpy as np
+import algorithm as al
+import sys
+print(sys.version)
+N=100
+
+data1 = np.arange(N)
+data2 = np.arange(50,N)
+
+# print(data1)
+# print(data2)
+print(np.cumsum(data1))
+print((np.arange(N) + 1))
+cumulative_average = np.cumsum(data1) / (np.arange(N) + 1)
+
+print(cumulative_average)
+
+""" print(al.Algorithm())
+print(al.eGreddy())
+print(al.UCB1())
+print(al.TS()) """
